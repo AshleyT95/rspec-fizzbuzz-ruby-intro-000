19fc411b795af7c19435d8eb4be39edfb67a667c
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-04-23 18:14:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 15 == 0
-    print "FizzBuzz"
+    puts "FizzBuzz"
   elsif int % 3 == 0
-    print "Fizz"
+    puts "Fizz"
   elsif int % 5 == 0
-    print "Buzz"
+    puts "Buzz"
   end
 end
